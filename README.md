11a8f9e452d42e76b3280e3a5e7bbd48df46012c
palermobruna4 <bruna.palermo2019@gmail.com>
2022-08-16 11:15:45 -0300

    projeto_criado
    Node js com package configurado, bibliotecas instaladas e index.js com a porta definida.

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # node_etim_tcc_glub
  Gerenciamento de lixo urbano brasileiro backend
+
+
---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require('express');
+
+const app = express();
+
+// const porta = process.env.Port || 3333;
+
+const porta = 3333;
+
+//define a porta do servidor - definir de acordo com a oferecida pelo serviço de hospedagem
+// 3333 pro node e 3000 pra react, porta não pode ser repetida
+app.listen(porta, () => {
+    console.log('Servidor iniciado na porta: ' + porta);
+})
