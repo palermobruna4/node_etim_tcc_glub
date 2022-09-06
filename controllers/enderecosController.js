cab39aa4d21d5677415ab7465c3caa74afe520c8
JhonatanDsJ <110039883+JhonatanDsJ@users.noreply.github.com>
2022-09-06 11:36:12 -0300

    listar endereco controller

--- controllers/enderecosController.js
+++ controllers/enderecosController.js
@@ -4,7 +4,10 @@ const db = require("../database/connection");
 module.exports = {
     async listarEnderecos(request, response){
         try{
-            return response.status(200).json({confirma: 'Enderecos'});
+            const sql ='Select use_id, end_logradouro, end_num, end_bairro, end_cep, cid_id from enderecos';
+            const enderecos = await db.query(sql);
+            return response.status(200).json({confirma: 'Sucesso', nResults: enderecos[0].length, mesage: enderecos[0]});
+           
         } catch(error){
             return response.status(500).json({confirma: 'Erro', message:error});
         }
--- controllers/postagensController.js
+++ controllers/postagensController.js
@@ -5,7 +5,9 @@ const db = require("../database/connection");
 module.exports = {
     async listarPostagens(request, response){
         try{
-            return response.status(200).json({confirma: 'Postagens'});
+            const sql ='select post_id, post_data, post_titulo, post_conteudo, post_destaque from postagens;';
+            const postagens = await db.query(sql);
+            return response.status(200).json({confirma: 'Sucesso', nResults: postagens[0].length, mesage: postagens[0]});
         } catch(error){
             return response.status(500).json({confirma: 'Erro', message:error});
         }
