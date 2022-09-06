238ec4a7c7beddb4a8e5315ec50d0b3b6a537550
Isabelly Pardinho :) <isabelly.pardinho@etec.sp.gov.br>
2022-09-06 11:36:59 -0300

    Listar imagens Controller

--- controllers/imagensController.js
+++ controllers/imagensController.js
@@ -5,7 +5,9 @@ const db = require("../database/connection");
 module.exports = {
     async listarImagens(request, response){
         try{
-            return response.status(200).json({confirma: 'Imagens'});
+            const sql = 'SELECT img_descricao, img_id, img_nome, post_id FROM imagens';
+            const imagens = await db.query(sql);
+            return response.status(200).json({confirma: 'Sucesso', nResults: imagens[0].length, menssage: imagens[0]});
         } catch(error){
             return response.status(500).json({confirma: 'Erro', message:error});
         }
