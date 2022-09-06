549ec50a5fa9861b7e18af50940d0f6b94c403a4
palermobruna4 <bruna.palermo2019@gmail.com>
2022-09-06 11:35:58 -0300

    listas mensagens controller

--- controllers/mensagensController.js
+++ controllers/mensagensController.js
@@ -5,7 +5,9 @@ const db = require("../database/connection");
 module.exports = {
     async listarMensagens(request, response){
         try{
-            return response.status(200).json({confirma: 'Mensagens'});
+            const sql= "select msm_id, msm_texto, pre_id, use_id, data from mensagens;";
+            const mensagens= await db.query(sql);
+            return response.status(200).json({confirma: 'Sucesso', nResults: mensagens[0] .lenght, mesage: mensagens[0]});
         } catch(error){
             return response.status(500).json({confirma: 'Erro', message:error});
         }
