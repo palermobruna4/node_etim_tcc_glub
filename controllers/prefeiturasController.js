4a7b590afc906e45a73d4d5e228ab2b37744f213
Isabela Batiston <isabela.batiston@etec.sp.gov.br>
2022-09-06 11:36:28 -0300

    Listar Prefeituras controller

--- controllers/prefeiturasController.js
+++ controllers/prefeiturasController.js
@@ -5,7 +5,9 @@ const db = require("../database/connection");
 module.exports = {
     async listarPrefeituras(request, response){
         try{
-            return response.status(200).json({confirma: 'Prefeituras'});
+            const sql = 'SELECT use_id, cid_id FROM prefeituras;';
+            const prefeituras = await db.query(sql);
+            return response.status(200).json({confirma: 'Sucesso', nResults: prefeituras[0].length, message: prefeituras[0]});
         } catch(error){
             return response.status(500).json({confirma: 'Erro', message:error});
         }
