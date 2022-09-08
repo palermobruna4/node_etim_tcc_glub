c95faaf12a3b800374faf1d728bef89e664fd66d
Isabela Batiston <isabela.batiston@etec.sp.gov.br>
2022-09-08 09:09:17 -0300

    Update comentariosController.js

--- controllers/comentariosController.js
+++ controllers/comentariosController.js
@@ -1,11 +1,13 @@
-//Isabela
+//Isabela e Isabelly
 const { json } = require("express");
 const db = require("../database/connection");
 
 module.exports = {
     async listarComentarios(request, response){
         try{
-            return response.status(200).json({confirma: 'Comentarios'});
+            const sql = 'SELECT coment_id, use_id, coment_texto, post_id, coment_pre_resposta, coment_status, coment_moderacao FROM comentarios';
+            const comentarios = await db.query(sql);
+            return response.status(200).json({confirma: 'Sucesso', nResults: comentarios[0].length, message: comentarios[0]});
         } catch(error){
             return response.status(500).json({confirma: 'Erro', message:error});
         }
